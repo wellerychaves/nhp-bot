b5be95f0daa7100945cbbbdbcd7378920fa5f072
wellerychaves <wellerychaves@outlook.com>
2025-03-20 16:31:29 -0300

    refactor: simplify the code to better reading

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,51 +1,37 @@
-const { REST, Routes } = require("discord.js");
-const path = require("node:path");
-const fs = require("node:fs");
+import { REST, Routes } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { TOKEN, CLIENT_ID } = process.env;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const commands = [];
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+// read all command files
+const commandFiles = fs.readdirSync(path.join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs
-		.readdirSync(commandsPath)
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-
-		if ("data" in command && "execute" in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(
-				`[AVISO] O comando da pasta ${filePath} está faltando a propriedade obrigatoria "data" ou "execute".`
-			);
-		}
-	}
+for (const file of commandFiles) {
+	const command = await import(`./commands/${file}`);
+	commands.push(command.data);
 }
 
-const rest = new REST().setToken(TOKEN);
+// initialize rest
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-(async () => {
+// register commands
+
+const registerCommands = async () => {
 	try {
-		console.log(
-			`Começou a atualizar ${commands.length} comandos de (/) do aplicativo`
-		);
+		console.log("Starting update of slash commands...");
 
-		const data = await rest.put(
-			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-			{ body: commands }
-		);
-
-		console.log(
-			`Carregado com sucesso ${data.length} comandos de (/) do aplicativo`
-		);
-	} catch (error) {
-		console.error(error);
+		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+		console.log("Slash commands registered successfully");
+	} catch (err) {
+		console.error(err);
 	}
-})();
+};
+
+registerCommands();
