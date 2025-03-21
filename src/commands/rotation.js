71335d15684812b30f76c9b383e57a2efaf2754a
wellerychaves <wellerychaves@outlook.com>
2025-03-21 11:07:01 -0300

    refactor(command): update the name to something more reasonable

--- src/commands/rotation.js
+++ src/commands/rotation.js
@@ -1,25 +1,25 @@
 import { SlashCommandBuilder } from "discord.js";
-import { getGroup, getNextGroupDate } from "../functions/getGroup";
+import { getNextRotation, getRotation } from "../functions/getGroup";
 
-const description = "Tells you which track group is currently in rotation";
+const description = "Returns which track rotation is currently on";
 
 export const data = new SlashCommandBuilder()
-	.setName("group")
+	.setName("rotation")
 	.setDescription(description)
 	.addIntegerOption((option) =>
 		option
 			.setMaxValue(20)
 			.setMinValue(1)
 			.setName("number")
-			.setDescription("Enter a number if you want to know the group's next appearance"),
+			.setDescription("Enter a number if you want to know the table's next appearance"),
 	);
 
 export async function execute(interaction) {
 	const interactionNumber = interaction.options.getInteger("number");
 
 	if (interactionNumber) {
-		interaction.reply(getNextGroupDate(interactionNumber));
+		interaction.reply(getNextRotation(interactionNumber));
 	} else {
-		interaction.reply(getGroup());
+		interaction.reply(getRotation());
 	}
 }
