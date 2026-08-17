b0752a77ef2d17c259451df7c5bdb8247b23f0be
Wellery Chaves <wellerychaves@outlook.com>
2026-08-17 13:50:08 -0300

    chore: remove all legacy JavaScript source files

--- src/events/ready.js
+++
@@ -1,6 +0,0 @@
-export const name = "clientReady";
-export const once = true;
-
-export function execute(client) {
-	console.log(`Bot online as ${client.user.tag}`);
-}
--- src/utils/convertSpeed.js
+++
@@ -1,4 +0,0 @@
-export const convertSpeed = (ms) => {
-	const kmh = ms * 3.6;
-	return Number.parseFloat(kmh.toFixed(2));
-};
